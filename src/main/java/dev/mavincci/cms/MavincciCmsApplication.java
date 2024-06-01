67edc207bdfd1cf51e69279e4b778e98a6ab9dc1
Michael Abebe <notthismichael@gmail.com>
2024-06-01 17:08:57 +0300

    INIT

---
+++ src/main/java/dev/mavincci/cms/MavincciCmsApplication.java
@@ -0,0 +1,13 @@
+package dev.mavincci.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavincciCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavincciCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mavincci/cms/MavincciCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mavincci.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavincciCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
